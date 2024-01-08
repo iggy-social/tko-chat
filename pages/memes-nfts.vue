ca9c9aafc13429192e35633e0e82118848732589
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2024-01-08 18:02:32 +0100

    chat sections for NFTs

---
+++ pages/memes-nfts.vue
@@ -0,0 +1,42 @@
+<template>
+  <Head>
+    <Title>Memes & NFTs | {{ $config.projectMetadataTitle }}</Title>
+    <Meta property="og:title" :content="'Memes & NFTs | ' + $config.projectMetadataTitle" />
+  </Head>
+
+  <ChatFeed 
+    class="mt-1" 
+    :showQuotedPost="$config.showRepliesOnHomepage" 
+    :allPosts="true" 
+    orbisContext="kjzl6cwe1jw146cone6md8rij8pe51xa33gyxyz599l2k4aadl4p6j990af1gxt" 
+  />
+</template>
+
+<script>
+import { useEthers } from 'vue-dapp'
+import ChatFeed from "../components/chat/ChatFeed.vue"
+
+export default {
+  name: "memesnfts",
+
+  components: {
+    ChatFeed
+  },
+
+  computed: {
+    getOrbisContext() {
+      if (this.$config.orbisTest) {
+        return this.$config.orbisTestContext;
+      } else {
+        return this.$config.orbisContext;
+      }
+    }
+  },
+
+  setup() {
+    const { address, chainId } = useEthers()
+
+    return { address, chainId }
+  }
+}
+</script>

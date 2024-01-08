1b71c4888ea1fd6c436bbe31b311cf90544869d6
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2024-01-08 17:58:08 +0100

    hide Mint button if no post minter contract

--- components/chat/ChatPost.vue
+++ components/chat/ChatPost.vue
@@ -92,7 +92,7 @@
           {{post.count_replies}}
         </NuxtLink>
 
-        <IggyPostMint :post="post" :parsedText="parsedText" v-if="isActivated" />
+        <IggyPostMint :post="post" :parsedText="parsedText" v-if="isActivated && $config.iggyPostMinterAddress" />
 
         <span v-if="post.master" class="cursor-pointer hover-color ms-2" data-bs-toggle="modal" :data-bs-target="'#replyModal'+post.stream_id">
           <i class="bi bi-reply" /> Reply
